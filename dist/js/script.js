962d4b7484b95bc6514d93140a9eb08c360eba92
Gvozdoedko <uraradhenko007@gmail.com>
2022-06-05 22:01:42 +0300

    DOM V1.1

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,8 +1,15 @@
-button.onclick = function() {
-    let textFromUser = document.getElementById('input_text').value;
-    console.log(textFromUser);
-    button.classList.add('vision');
-    input_text.classList.add('vision');
-    answer.textContent = `Hello, ${textFromUser}!`
-};
+function main() {
+    let input = document.body.children[2].children[0];
+    let button = document.body.children[2].children[1];
+    let span = document.body.children[2].children[2];
 
+    button.onclick = function () {
+        let textFromUser = document.getElementById("input_text").value;
+        console.log(textFromUser);
+        input.classList.add("vision");
+        button.classList.add("vision");
+        span.textContent = `Hello, ${textFromUser}!`;
+    };
+}
+
+main();
